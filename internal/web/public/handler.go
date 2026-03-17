0cb4419d5987fc65b619c800a02b1e144b401edf
liyongjie <yongjie_li_hnxc@msn.com>
2026-03-17 16:02:37 +0800

    Add HTTP handler for serving M3U files

---
+++ internal/web/public/handler.go
@@ -0,0 +1,21 @@
+package public
+
+import (
+    "database/sql"
+    "net/http"
+    "os"
+    "path/filepath"
+)
+
+type Handler struct {
+    db *sql.DB
+}
+
+func NewHandler(db *sql.DB) *Handler {
+    return &Handler{db: db}
+}
+
+func (h *Handler) ServeM3U(w http.ResponseWriter, r *http.Request) {
+    // 直接返回生成好的文件
+    http.ServeFile(w, r, filepath.Join("outfile", "live.m3u"))
+}
