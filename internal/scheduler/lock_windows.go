ebe1fd573dd0c08a92b7c7eb74814812dccfa819
liyongjie <yongjie_li_hnxc@msn.com>
2026-05-09 22:47:42 +0800

    Update lock_windows.go

--- internal/scheduler/lock_windows.go
+++ internal/scheduler/lock_windows.go
@@ -1,24 +1,17 @@
 //go:build windows
-
-// internal/scheduler/lock_windows.go
-// 修正 Windows 锁：引入 golang.org/x/sys/windows 包，修复参数错误。
 package scheduler
 
 import (
 	"os"
-
 	"golang.org/x/sys/windows"
 )
 
-// LockFile 在 Windows 上对文件加排他锁（非阻塞）。
 func LockFile(f *os.File) error {
 	h := windows.Handle(f.Fd())
 	var ol windows.Overlapped
-	flags := uint32(windows.LOCKFILE_EXCLUSIVE_LOCK | windows.LOCKFILE_FAIL_IMMEDIATELY)
-	return windows.LockFileEx(h, flags, 0, 0, 0, &ol)
+	return windows.LockFileEx(h, windows.LOCKFILE_EXCLUSIVE_LOCK|windows.LOCKFILE_FAIL_IMMEDIATELY, 0, 0, 0, &ol)
 }
 
-// UnlockFile 释放文件锁。
 func UnlockFile(f *os.File) error {
 	h := windows.Handle(f.Fd())
 	var ol windows.Overlapped
